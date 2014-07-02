2e0f53e57a0566e86801172a564faebf2a5d4c1d
James Hurley <jamesghurley@gmail.com>
2014-07-01 20:02:09 -0700

    Add API

---
+++ api/Apple/VCSimpleSession.m
@@ -0,0 +1,13 @@
+//
+//  VCSimpleSession.m
+//  VideoCoreDevelopment
+//
+//  Created by James Hurley on 7/1/14.
+//  Copyright (c) 2014 VideoCore. All rights reserved.
+//
+
+#import "VCSimpleSession.h"
+
+@implementation VCSimpleSession
+
+@end
