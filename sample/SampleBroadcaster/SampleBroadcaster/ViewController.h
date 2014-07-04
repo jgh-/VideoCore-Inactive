1b319cd13573597e63037f567f47ed97fc9755ff
James Hurley <jamesghurley@gmail.com>
2014-07-04 13:39:42 -0700

    Update sample project to use new simple api

--- sample/SampleBroadcaster/SampleBroadcaster/ViewController.h
+++ sample/SampleBroadcaster/SampleBroadcaster/ViewController.h
@@ -9,16 +9,13 @@
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 
-#import "SampleGraph.h"
 
 @interface ViewController : UIViewController
-{
-    std::unique_ptr<videocore::sample::SampleGraph> _sampleGraph;
-}
 
-@property (retain, nonatomic) IBOutlet UIButton *btnSpin;
-@property (retain, nonatomic) IBOutlet GLKView *glkView;
+
+@property (retain, nonatomic) IBOutlet UIView *previewView;
 @property (retain, nonatomic) IBOutlet UIButton *btnConnect;
+
 - (IBAction)btnConnectTouch:(id)sender;
-- (IBAction)btnSpinTouch:(id)sender;
+
 @end
