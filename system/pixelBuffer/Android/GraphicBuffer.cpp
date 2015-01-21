1fd084364c425cae90b2783ebed733841fef5070
James Hurley <jamesghurley@gmail.com>
2015-01-20 17:01:23 -0800

    Add GraphicBuffer wrapper

---
+++ system/pixelBuffer/Android/GraphicBuffer.cpp
@@ -0,0 +1,71 @@
+#include <videocore/system/pixelBuffer/Android/GraphicBuffer.h>
+#include <dlfcn.h>
+
+namespace videocore { namespace Android {
+    typedef android::status_t (*pfnGraphicsBufferLock)(void*, uint32_t usage, void** addr);
+    typedef android::status_t (*pfnGraphicsBufferUnlock)(void*);
+    typedef android_native_buffer_t* (*pfnGraphicsBufferGetNativeBuffer)(void*);
+    typedef void (*pfnGraphicsBufferCtor)(void*, uint32_t w, uint32_t h, android::PixelFormat f, uint32_t u);
+    typedef void (*pfnGraphicsBufferDtor)(void*);
+
+
+    pfnGraphicsBufferLock fGraphicsBufferLock;
+    pfnGraphicsBufferUnlock fGraphicsBufferUnlock;
+    pfnGraphicsBufferGetNativeBuffer fGraphicsBufferGetNativeBuffer;
+    pfnGraphicsBufferCtor fGraphicsBufferCtor;
+    pfnGraphicsBufferDtor fGraphicsBufferDtor;
+
+    bool GraphicBuffer::s_isSetup = false;
+
+    void
+    GraphicBuffer::setup()
+    {
+        if(!s_isSetup)
+        {
+            s_isSetup = true;
+            void* handle = dlopen("libui.so", RTLD_LAZY);
+
+            fGraphicsBufferLock = (pfnGraphicsBufferLock)dlsym(handle, "_ZN7android13GraphicBuffer4lockEjPPv");
+            fGraphicsBufferUnlock = (pfnGraphicsBufferUnlock)dlsym(handle, "_ZN7android13GraphicBuffer6unlockEv");
+            fGraphicsBufferGetNativeBuffer = (pfnGraphicsBufferGetNativeBuffer)dlsym(handle, "_ZNK7android13GraphicBuffer15getNativeBufferEv");
+            fGraphicsBufferCtor = (pfnGraphicsBufferCtor)dlsym(handle, "_ZN7android13GraphicBufferC1Ejjij");
+            fGraphicsBufferDtor = (pfnGraphicsBufferDtor)dlsym(handle, "_ZN7android13GraphicBufferD1Ev");
+
+        }
+    }
+
+    GraphicBuffer::GraphicBuffer(uint32_t width, uint32_t height, android::PixelFormat format, uint32_t usage)
+    : m_width(width), m_height(height), m_format(format)
+    {
+    	GraphicBuffer::setup();
+
+    	m_buffer = malloc(sizeof(android::GraphicBuffer));
+    	fGraphicsBufferCtor(m_buffer, width, height, format, usage);
+
+    }
+    GraphicBuffer::~GraphicBuffer() 
+    {
+    	fGraphicsBufferDtor(m_buffer);
+    	free(m_buffer);
+    	m_buffer = nullptr;
+    }
+    
+    android::status_t
+    GraphicBuffer::lock(uint32_t usage, void** addr)
+    {
+    	return fGraphicsBufferLock(m_buffer, usage, addr);
+    }
+
+    android::status_t
+    GraphicBuffer::unlock()
+    {
+    	return fGraphicsBufferUnlock(m_buffer);
+    }
+
+    android_native_buffer_t*
+    GraphicBuffer::getNativeBuffer()
+    {
+    	return fGraphicsBufferGetNativeBuffer(m_buffer);
+    }
+
+}}
---
+++ system/pixelBuffer/Android/GraphicBuffer.h
@@ -0,0 +1,62 @@
+/*
+ 
+ Video Core
+ Copyright (c) 2014 James G. Hurley
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ */
+#ifndef videocore_Android_GraphicBuffer_h
+#define videocore_Android_GraphicBuffer_h
+#include <stdint.h>
+#include <ui/GraphicBuffer.h>
+
+namespace videocore { namespace Android {
+
+	class GraphicBuffer
+	{	
+
+	public:
+		GraphicBuffer(uint32_t width, uint32_t height, android::PixelFormat format, uint32_t usage);
+		~GraphicBuffer();
+
+		android::status_t lock(uint32_t usage, void** addr);
+		android::status_t unlock();
+		android_native_buffer_t* getNativeBuffer();
+
+		const uint32_t getWidth() const { return m_width; };
+		const uint32_t getHeight() const { return m_height; };
+		const android::PixelFormat getPixelFormat() const { return m_format; };
+		
+    private: 
+        static void setup();
+        static bool s_isSetup;
+
+    private:
+    	void* m_buffer;
+
+    	uint32_t m_width;
+    	uint32_t m_height;
+    	android::PixelFormat m_format;
+
+	};
+
+}}
+
+#endif
