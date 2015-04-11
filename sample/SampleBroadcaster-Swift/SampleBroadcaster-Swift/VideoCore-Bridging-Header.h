57618c198757b03f873fa542175658e1dfb70199
Josh Lieberman <josh.lieberman92@gmail.com>
2015-04-11 14:54:56 -0400

    Initial setup of swift example.

---
+++ sample/SampleBroadcaster-Swift/SampleBroadcaster-Swift/VideoCore-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  VideoCore-Bridging-Header.h
+//  SampleBroadcaster-Swift
+//
+//  Created by Josh Lieberman on 4/11/15.
+//  Copyright (c) 2015 videocore. All rights reserved.
+//
+
+#ifndef SampleBroadcaster_Swift_VideoCore_Bridging_Header_h
+#define SampleBroadcaster_Swift_VideoCore_Bridging_Header_h
+
+#import "VCSimpleSession.h"
+
+#endif
---
+++ sample/SampleBroadcaster-Swift/SampleBroadcaster-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleBroadcaster-Swift
+//
+//  Created by Josh Lieberman on 4/11/15.
+//  Copyright (c) 2015 videocore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
